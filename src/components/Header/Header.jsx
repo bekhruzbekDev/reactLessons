57c8b1ba712330d23dd71d0e0be2dec1a872482e
bekruz-pardayev <behruzp330@gmail.com>
2024-06-11 15:43:07 +0500

    lesson-3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,21 @@
 import "./header.css";
 //svg import
-import Logo from "../../assets/react.svg"
+import Logo from "../../assets/react.svg";
+import { NavLink } from "react-router-dom";
 export const Header = () => {
   return (
     <header>
-      <Logo/>
+      <Logo />
       <ul>
-        <li>About</li>
+      <NavLink to="/">
+          <li>Home</li>
+        </NavLink>
+        <NavLink to="/about">
+          <li>About</li>
+        </NavLink>
+        <NavLink to="/contact">
         <li>Contact</li>
+        </NavLink>
       </ul>
     </header>
   );
